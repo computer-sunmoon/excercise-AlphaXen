ccf5213263f3a043660c2bd1ed4c896992b8a8c8
AlphaXen <kimminjae771@naver.com>
2025-09-03 01:22:48 +0900

    Fix: Correctly format Text widget in tutorial01.dart

--- lib/week01/tutorial01.dart
+++ lib/week01/tutorial01.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center
-    (child: Text(key: 'Helllo, World!',
-    textDirection: TextDirection.ltr,
+    const Center(
+      child: Text(key: 'Helllo, World!',
+      textDirection: TextDirection.ltr,
     style: TextStyle(fontSize: 32, color: Colors.white),
     ),
     ;)
