176fe7ebeef434517e6ff40cdfae73bd7e694db0
AlphaXen <kimminjae771@naver.com>
2025-09-03 01:22:17 +0900

    Fix: Add white color to the 'Hello, World!' text.

--- lib/week01/tutorial01.dart
+++ lib/week01/tutorial01.dart
@@ -5,5 +5,7 @@ void main() {
     const Center
     (child: Text(key: 'Helllo, World!',
     textDirection: TextDirection.ltr,
-    style: TextStyle(fontSize: 32)))
+    style: TextStyle(fontSize: 32, color: Colors.white),
+    ),
+    ;)
 }
